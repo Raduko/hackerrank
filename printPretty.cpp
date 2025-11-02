39aae922e1ed65abee71cf68ebfb3f1e1442a546
Selim Enes DÜNDAR <32342275+Raduko@users.noreply.github.com>
2025-11-02 15:45:43 +0300

    Create printPretty.cpp

---
+++ printPretty.cpp
@@ -0,0 +1,3 @@
+cout << showbase << hex << left << nouppercase << long(A) << endl;
+cout << setw(15) << right << setfill('_') << showpos << fixed << setprecision(2) << B << endl;
+cout << scientific << noshowpos << uppercase << setprecision(9) << C << endl;
